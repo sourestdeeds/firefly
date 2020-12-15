a499d0e54fd233cdfa2684a31994809dc5f7e3ae
sourestdeeds <stevemindoza@gmail.com>
2020-12-15 01:44:46 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,25 @@
+#!/usr/bin/env python3
+
+import os
 from setuptools import setup
+
+directory = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(directory, 'README.md'), encoding='utf-8') as f:
+  long_description = f.read()
+
 setup(name='retriever',
-version='1.0.0',
-description='For use with TransitFit',
-url='https://github.com/sourestdeeds/retrieval',
-author='Steven Charles-Mindoza',
-author_email='stevemindoza@gmail.com',
-license='GPL-3.0 Licence',
-packages=['retriever'],
-python_requires='>=3.6',
-install_requires=['batman-package', 'dynesty', 'numpy', 'matplotlib',
+      version='1.0.0',
+      description='For use with TransitFit',
+      author='Steven Charles-Mindoza',
+      license='MIT',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      packages = ['retriever'],
+      classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License"
+      ],
+      install_requires=['batman-package', 'dynesty', 'numpy', 'matplotlib',
                       'pandas', 'ldtk', 'lightkurve', 'transitfit'],
-zip_safe=False)
+      python_requires='>=3.6',
+      include_package_data=True)
