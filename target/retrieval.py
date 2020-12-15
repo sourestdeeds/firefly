07b0512a1a697dcbb5c7cea9dc7cd6ee8c4c5387
sourestdeeds <stevemindoza@gmail.com>
2020-12-15 23:36:09 +0000

    v0.5.7

--- target/retrieval.py
+++ target/retrieval.py
@@ -675,9 +675,23 @@ def auto_retrieval(targets, processes=len(os.sched_getaffinity(0)) // 4,
     employs TransitFit across each TESS sector for every exoplanet in the list given.
     Runs TransitFit for all available split curves.
     
+    Example:
+
+    - For a single target
+        
+        target = ('WASP-43 b',)
+        
+        auto_retrieval(target)
+
+    - For a list of targets
+        
+        targets = ('WASP-43 b', 'WASP-18 b', 'WASP-91 b')
+        
+        auto_retrieval(targets)
+    
     Parameters
     ----------
-    targets : str
+    targets : str, list
         A list of exoplanet targets.
     processes : int, optional
         The number of processes to run in parallel. For UNIX, this default
@@ -693,9 +707,9 @@ def auto_retrieval(targets, processes=len(os.sched_getaffinity(0)) // 4,
         A list of different detrending models. Each entry should consist
         of a method and a second parameter dependent on the method.
         Accepted methods are
-            ['nth order', order]
-            ['custom', function, [global fit indices, filter fit indices, epoch fit indices]]
-            ['off', ]
+        - ['nth order', order]
+        - ['custom', function, [global fit indices, filter fit indices, epoch fit indices]]
+        - ['off', ]
         function here is a custom detrending function. TransitFit assumes
         that the first argument to this function is times and that all
         other arguments are single-valued - TransitFit cannot fit
@@ -717,12 +731,13 @@ def auto_retrieval(targets, processes=len(os.sched_getaffinity(0)) // 4,
     dynesty_sample : str, optional
         Method used to sample uniformly within the likelihood constraint,
         conditioned on the provided bounds. Unique methods available are:
-        uniform sampling within the bounds('unif'), random walks with fixed
-        proposals ('rwalk'), random walks with variable ("staggering")
-        proposals ('rstagger'), multivariate slice sampling along preferred
-        orientations ('slice'), "random" slice sampling along all
-        orientations ('rslice'), "Hamiltonian" slices along random
-        trajectories ('hslice'), and any callable function which follows
+        - uniform sampling within the bounds('unif') 
+        - random walks with fixed proposals ('rwalk') 
+        - random walks with variable ("staggering") proposals ('rstagger') 
+        - multivariate slice sampling along preferred orientations ('slice') 
+        - "random" slice sampling along all orientations ('rslice') 
+        - "Hamiltonian" slices along random trajectories ('hslice') 
+        and any callable function which follows
         the pattern of the sample methods defined in dynesty.sampling.
         'auto' selects the sampling method based on the dimensionality of
         the problem (from ndim). When ndim < 10, this defaults to 'unif'.
