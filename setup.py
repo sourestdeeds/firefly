7181c4858e395a0cbb48f3e9c2d3253e9bb960b9
sourestdeeds <stevemindoza@gmail.com>
2020-12-15 01:39:29 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,4 +7,7 @@ author='Steven Charles-Mindoza',
 author_email='stevemindoza@gmail.com',
 license='Open GL 3.0',
 packages=['retriever'],
+python_requires='>=3.6',
+install_requires=['batman-package', 'dynesty', 'numpy', 'matplotlib',
+                      'pandas', 'ldtk', 'lightkurve', 'transitfit'],
 zip_safe=False)
