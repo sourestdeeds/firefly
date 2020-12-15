afdc8191d73c2c2fa5f84ecfe8e2b9e428facf4f
sourestdeeds <stevemindoza@gmail.com>
2020-12-15 12:09:48 +0000

    v1.5.6

--- setup.py
+++ setup.py
@@ -10,14 +10,14 @@ directory = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(directory, 'README.md'), encoding='utf-8') as f:
   long_description = f.read()
 
-setup(name='retriever',
+setup(name='retrieval',
       version='1.0.0',
       description='For use with TransitFit',
       author='Steven Charles-Mindoza',
       license='MIT',
       long_description=long_description,
       long_description_content_type='text/markdown',
-      packages = ['retriever'],
+      packages = ['retrieval'],
       classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License"
