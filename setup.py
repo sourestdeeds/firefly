72b58156300a5aef811ac9217881d17f36ac631a
sourestdeeds <stevemindoza@gmail.com>
2020-12-15 01:36:15 +0000

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(name='retriever',
+version='1.0.0',
+description='For use with TransitFit',
+url='TBA',
+author='Steven Charles-Mindoza',
+author_email='stevemindoza@gmail.com',
+license='Open GL 3.0',
+packages=['retriever'],
+zip_safe=False)
