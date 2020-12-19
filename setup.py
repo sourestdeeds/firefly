5ba4cf81b1cf5d7715d654b23b540d10bb13a17a
Steven Charles-Mindoza <stevemindoza@gmail.com>
2020-12-19 11:58:45 +0000

    v0.6.1

--- setup.py
+++ setup.py
@@ -2,29 +2,29 @@
 
 import os
 import pathlib
-from setuptools import setup, find_packages
+import setuptools
 
 here = pathlib.Path(__file__).parent.resolve()
 
 directory = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(directory, 'README.md'), encoding='utf-8') as f:
-  long_description = f.read()
+    long_description = f.read()
 
-setup(name='firefly',
-      version='0.6.1',
-      description='For use with TransitFit',
-      author='Steven Charles-Mindoza',
-      license='MIT',
-      long_description=long_description,
-      long_description_content_type='text/markdown',
-      packages=find_packages(),
-      classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License"
-      ],
-      install_requires=['numpy',
-                        'pandas', 
-                        'lightkurve',
-                        'transitfit'],
-      python_requires='>=3.6',
-      include_package_data=True)
+setuptools.setup( name='firefly',
+                  version=get_version('firefly/__init__.py'),
+                  description='For use with TransitFit',
+                  author='Steven Charles-Mindoza',
+                  license='MIT',
+                  long_description=long_description,
+                  long_description_content_type='text/markdown',
+                  packages=find_packages(),
+                  classifiers=[
+                    "Programming Language :: Python :: 3",
+                    "License :: OSI Approved :: MIT License"
+                  ],
+                  install_requires=['numpy',
+                                    'pandas', 
+                                    'lightkurve',
+                                    'transitfit'],
+                  python_requires='>=3.6',
+                  include_package_data=True )
