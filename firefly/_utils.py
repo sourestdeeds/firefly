a7fb5a3ada1bd335f12e204a809da3c37face176
Steven Charles-Mindoza <stevemindoza@gmail.com>
2020-12-21 12:39:44 +0000

    v0.6.3

--- firefly/_utils.py
+++ firefly/_utils.py
@@ -1,9 +1,9 @@
 from transitfit import split_lightcurve_file, run_retrieval, calculate_logg
 try:
-    from lightcurve import search_lightcurve
+    from lightkurve import search_lightcurve
 except:
     try:
-        from lightcurve import search_lightcurvefile as search_lightcurve
+        from lightkurve import search_lightcurvefile as search_lightcurve
     except:
         raise
 from traceback import format_exc
