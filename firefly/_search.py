8a4bc8a141800a85b1c31e843a6a8719ca176d7d
Steven Charles-Mindoza <stevemindoza@gmail.com>
2020-12-21 21:24:16 +0000

    v0.6.5

--- firefly/_search.py
+++ firefly/_search.py
@@ -16,14 +16,12 @@ from pandas import read_csv
 
 def _fuzzy_search(exoplanet, archive='eu'):
     if archive == 'eu':
-        with suppress_print():
-            _download_eu()
+        _download_eu()
         eu_csv = 'firefly/data/eu.csv'
         exo_list = read_csv(eu_csv, usecols=['# name']).values.tolist()
         exo_list = [j for i in exo_list for j in i]
     elif archive == 'nasa':
-        with suppress_print():
-            _download_nasa()
+        _download_nasa()
         nasa_csv = 'firefly/data/nasa.csv'
         exo_list = read_csv(nasa_csv, usecols=['pl_name']).values.tolist()
         exo_list = [j for i in exo_list for j in i]
