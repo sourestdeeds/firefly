1e310874194fe6c3de16d98bc09b08967995b9e9
Steven Charles-Mindoza <stevemindoza@gmail.com>
2020-12-21 19:32:49 +0000

    v0.6.5

---
+++ firefly/query.py
@@ -0,0 +1,91 @@
+from ._search import _fuzzy_search
+from ._archive import _eu, _nasa
+
+try:
+    from lightkurve import search_lightcurve
+except:
+    try:
+        from lightkurve import search_lightcurvefile as search_lightcurve
+    except:
+        raise
+from tabulate import tabulate
+from shutil import rmtree
+import sys
+import os
+
+
+
+def query(target, archive='eu'):
+    '''
+    Performs a name search and returns the 5 top matches. 
+    Parameters
+    ----------
+    exoplanet : str
+        The exoplanet target to retreive information for.
+    archive : str, optional
+        The exoplanet archive to use for priors. The default is 'eu'.
+
+    Returns
+    -------
+    Data printed to console.
+
+    '''
+    output = _fuzzy_search(target, archive='eu')
+    table = output[1]
+    print(tabulate(table, tablefmt='psql', headers=['Exoplanet', '% Match']))
+
+
+def archive_query(target, archive='eu'):
+    '''
+    Performs a query for prior information.
+
+    Parameters
+    ----------
+    exoplanet : str
+        The exoplanet target to retreive information for.
+    archive : str, optional
+        The exoplanet archive to use for priors. The default is 'eu'.
+
+    Returns
+    -------
+    Data printed to console.
+
+    '''
+    highest, ratios = _fuzzy_search(target, archive='eu')
+    exoplanet = highest[0]
+    temp = f'firefly/{exoplanet}'
+    os.makedirs(temp, exist_ok=True)
+    if archive == 'eu':
+        _eu(exoplanet)
+    elif archive == 'nasa':
+        _nasa(exoplanet)
+    rmtree(temp)
+
+def mast_query(target, archive='eu'):
+    '''
+    Performs a query for data products from MAST.
+
+    Parameters
+    ----------
+    exoplanet : str
+        The exoplanet target to retreive information for.
+    archive : str, optional
+        The exoplanet archive to use for priors. The default is 'eu'.
+
+    Returns
+    -------
+    Data printed to console.
+
+    '''
+    highest, ratios = _fuzzy_search(target, archive='eu')
+    exoplanet = highest[0]
+    lc = search_lightcurve(exoplanet, mission='TESS')
+    if len(lc) == 0:
+        sys.exit(f'Search result contains no data products for {exoplanet}.')
+    lc = lc .table .to_pandas()[['observation', 
+                                 'productFilename', 'size', 't_exptime']] \
+            .rename(columns={'observation':'Observation'}) \
+            .rename(columns={'size':'Size'}) \
+            .rename(columns={'productFilename':'Product'}) 
+    lc = lc[lc.t_exptime != 20].drop(['t_exptime'], axis=1)
+    print(tabulate(lc, tablefmt='psql', showindex=False, headers='keys'))
