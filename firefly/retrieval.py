ce03b6bc1868d3eb79aa8520cdb985bd0498ddee
Steven Charles-Mindoza <stevemindoza@gmail.com>
2020-12-21 15:44:10 +0000

    v0.6.4

--- firefly/retrieval.py
+++ firefly/retrieval.py
@@ -694,7 +694,7 @@ def auto_retrieval(targets, processes=len(os.sched_getaffinity(0)) // 4,
     except BaseException:
         exo_folder = f'firefly/{exoplanet}'
         now = datetime.now().strftime("%d-%b-%Y %H:%M:%S")
-        make_archive(f'{exo_folder} {now} KeyboardInterrupt', format='gztar',
+        make_archive(f'{exo_folder} {now} BaseException', format='gztar',
                  root_dir=f'{os.getcwd()}/firefly/',
                  base_dir=f'{exoplanet}')
         rmtree(f'{exo_folder}')
