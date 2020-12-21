da4f43290d9711c16024aea46b9a29a8b659eecc
Steven Charles-Mindoza <stevemindoza@gmail.com>
2020-12-21 19:30:57 +0000

    v0.6.5

--- firefly/__init__.py
+++ firefly/__init__.py
@@ -1,4 +1,5 @@
 name = 'firefly'
-__version__ = '0.6.4'
+__version__ = '0.6.5'
 
-from .retrieval import query, retrieval, auto_retrieval
+from .retrieval import retrieval, auto_retrieval
+from .query import query, archive_query, mast_query
