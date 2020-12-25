42b7b914a7c4baf0b504fa97b352c812a9656263
Steven Charles-Mindoza <stevemindoza@gmail.com>
2020-12-25 22:23:36 +0000

    v0.6.9

--- firefly/_search.py
+++ firefly/_search.py
@@ -17,12 +17,14 @@ def _fuzzy_search(exoplanet, archive='eu'):
     if archive == 'eu':
         _download_eu()
         eu_csv = 'firefly/data/eu.csv'
-        exo_list = read_csv(eu_csv, usecols=['# name']).values.tolist()
+        exo_list = read_csv(eu_csv, usecols=['# name']) \
+                   .drop_duplicates() .values .tolist()
         exo_list = [j for i in exo_list for j in i]
     elif archive == 'nasa':
         _download_nasa()
         nasa_csv = 'firefly/data/nasa.csv'
-        exo_list = read_csv(nasa_csv, usecols=['pl_name']).values.tolist()
+        exo_list = read_csv(nasa_csv, usecols=['pl_name']) \
+                   .drop_duplicates() .values .tolist()
         exo_list = [j for i in exo_list for j in i]
     ratios = process.extract(exoplanet,exo_list)
     highest = process.extractOne(exoplanet,exo_list)
