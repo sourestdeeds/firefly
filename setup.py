67ce64448896014912fc16145de4c5959fe19a3d
Steven Charles-Mindoza <stevemindoza@gmail.com>
2021-01-07 00:53:54 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -38,6 +38,7 @@ setuptools.setup( name='firefly',
                                     'tabulate',
                                     'fuzzywuzzy',
                                     'python-Levenshtein',
+                                    'pydrive',
                                     'transitfit'],
                   python_requires='>=3.6',
                   include_package_data=True )
