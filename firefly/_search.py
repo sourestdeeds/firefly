7af6f6977b4641dadabb7f185a98c24ddbba7f7d
Steven Charles-Mindoza <stevemindoza@gmail.com>
2021-01-15 00:19:33 +0000

    v0.7.5

--- firefly/_search.py
+++ firefly/_search.py
@@ -7,26 +7,20 @@ Spelling checker for the search engine.
 """
 
 
-from ._archive import _download_eu, _download_nasa
+from ._archive import _download_nasa
 
 from fuzzywuzzy import process
 from pandas import read_csv
 
 
-def _fuzzy_search(exoplanet, archive='eu'):
-    if archive == 'eu':
-        _download_eu()
-        eu_csv = 'firefly/data/eu.csv.gz'
-        exo_list = read_csv(eu_csv, usecols=['# name']) \
-                   .drop_duplicates() .values .tolist()
-        exo_list = [j for i in exo_list for j in i]
-    elif archive == 'nasa':
-        _download_nasa()
-        nasa_csv = 'firefly/data/nasa.csv.gz'
-        exo_list = read_csv(nasa_csv, usecols=['pl_name', 'tic_id']) \
-                   .dropna() .drop_duplicates('pl_name') \
-                   .drop(['tic_id'], axis=1) .values .tolist()
-        exo_list = [j for i in exo_list for j in i]
+def _fuzzy_search(exoplanet):
+    _download_nasa()
+    nasa_csv = 'firefly/data/nasa.csv.gz'
+    exo_list = read_csv(nasa_csv, usecols=['pl_name', 'tic_id']) \
+               .dropna() .drop_duplicates('pl_name') \
+               .drop(['tic_id'], axis=1) .values .tolist()
+    exo_list = [j for i in exo_list for j in i]
+    
     ratios = process.extract(exoplanet,exo_list)
     highest = process.extractOne(exoplanet,exo_list)
     return highest, ratios
