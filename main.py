b1903b9898e70a65ade3450435083270da013501
Steven Charles-Mindoza <stevemindoza@gmail.com>
2021-01-26 00:24:34 +0000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,34 @@
+from firefly import firefly, tess_viable
+
+
+def main():
+    exoplanet=tess_viable(k=5)
+    firefly(
+        # Firefly Interface
+        exoplanet, 
+        curve_sample=0.01, 
+        email=True,
+        to=['transitfit.server@gmail.com'], 
+        clean=False,
+        cache=True,
+        # TransitFit Variables
+        cutoff=0.25,
+        window=2,
+        nlive=500, 
+        fit_ttv=False,
+        detrending_list=[['nth order', 2]],
+        dynesty_sample='rwalk', 
+        fitting_mode='folded',
+        limb_darkening_model='quadratic', 
+        ld_fit_method='coupled',
+        max_batch_parameters=25, 
+        batch_overlap=2, 
+        dlogz=None, 
+        maxiter=None, 
+        maxcall=None, 
+        dynesty_bounding='multi', 
+        normalise=True, 
+        detrend=True
+    )
+
+main()
