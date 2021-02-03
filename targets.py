99ab150412baab03fea798b4ffea23b759776d14
Steven Charles-Mindoza <stevemindoza@gmail.com>
2021-02-03 00:03:51 +0000

    Update targets.py

--- targets.py
+++ targets.py
@@ -1,8 +1,8 @@
-from firefly import tess_viable
+from firefly import tess
 from subprocess import run
 
 def targets():
-    targets, all_targets, ttv_targets = tess_viable(k=25, survey='WASP')
+    targets, all_targets, ttv_targets = tess(k=25, survey='WASP')
     all_targets = ['lhs1815b', 'wasp119b', 'toi157b', 'kepler42c', 'hip65Ab',
                    'l9859b', 'gj1252b', 'wasp62b', 'hd213885b', 'qatar1b']
     for i, exoplanet in enumerate(all_targets):
