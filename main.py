ebdea33f8b14ac0c4c6ddd58ed3ce9b1754f987a
Steven Charles-Mindoza <stevemindoza@gmail.com>
2021-02-03 18:13:57 +0000

    v0.7.7

--- main.py
+++ main.py
@@ -15,41 +15,49 @@ class suppress_print():
         
 
 def main(exoplanet):
-    with suppress_print():
-        firefly(
-        # Firefly Interface
-        exoplanet,
-        archive='eu',
-        curve_sample=1,
-        email=False,
-        to=['transitfit.server@gmail.com'],
-        clean=True,
-        cache=False,
-        auto=True,
-        # TransitFit Variables
-        cutoff=0.25,
-        window=2.5,
-        nlive=1000,
-        fit_ttv=False,
-        detrending_list=[['nth order', 2]],
-        dynesty_sample='rslice',
-        fitting_mode='folded',
-        limb_darkening_model='quadratic',
-        ld_fit_method='coupled',
-        max_batch_parameters=25,
-        batch_overlap=2,
-        dlogz=None,
-        maxiter=None,
-        maxcall=None,
-        dynesty_bounding='multi',
-        normalise=True,
-        detrend=True
+    firefly(
+    # Firefly Interface
+    exoplanet,
+    archive='eu',
+    curve_sample=1,
+    email=False,
+    to=['transitfit.server@gmail.com'],
+    clean=True,
+    cache=False,
+    auto=True,
+    # TransitFit Variables
+    cutoff=0.25,
+    window=2.5,
+    nlive=1000,
+    fit_ttv=False,
+    detrending_list=[['nth order', 2]],
+    dynesty_sample='rslice',
+    fitting_mode='folded',
+    limb_darkening_model='quadratic',
+    ld_fit_method='coupled',
+    max_batch_parameters=25,
+    batch_overlap=2,
+    dlogz=None,
+    maxiter=None,
+    maxcall=None,
+    dynesty_bounding='multi',
+    normalise=True,
+    detrend=True
     )
 
+
+processes = 1
 targets, all_targets, ttv_targets = tess(archive='eu', survey='WASP')
-all_targets = ['wasp126b', 'lhs1815b', 'kepler42c', 'wasp119b', 'toi157b',
-               'wasp18b', 'hip65ab', 'l9859b', 'gj1252b', 'wasp62b']
+all_targets = [
+    'wasp100b', 'wasp126b', 'lhs1815b', 'kepler42c', 'wasp119b',
+    'wasp18b', 'hip65ab', 'l9859b', 'gj1252b', 'wasp62b',
+    'toi157b',
+]
 
 if __name__ == '__main__':
-    with Pool(processes=2) as pool:
-        pool.map(main, all_targets)
+    with Pool(processes=processes) as pool:
+        if processes==1:
+            pool.map(main, all_targets)
+        else:
+            with suppress_print():
+                pool.map(main, all_targets)
