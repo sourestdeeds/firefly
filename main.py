ec6fbfdb123a6863982351e0778e55d6e5a473f1
Steven Charles-Mindoza <stevemindoza@gmail.com>
2021-02-04 14:11:16 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,37 +1,48 @@
 from firefly import firefly
 from sys import argv
+import sys, os
+
+class suppress_print():
+    def __enter__(self):
+        self.original_stdout = sys.stdout
+        sys.stdout = open(os.devnull, 'w')
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        sys.stdout.close()
+        sys.stdout = self.original_stdout
         
 
 def main(exoplanet):
-    firefly(
-    # Firefly Interface
-    exoplanet,
-    archive='eu',
-    curve_sample=1,
-    email=False,
-    to=['transitfit.server@gmail.com'],
-    clean=True,
-    cache=False,
-    auto=True,
-    # TransitFit Variables
-    cutoff=0.25,
-    window=2.5,
-    nlive=1000,
-    fit_ttv=False,
-    detrending_list=[['nth order', 2]],
-    dynesty_sample='rslice',
-    fitting_mode='folded',
-    limb_darkening_model='quadratic',
-    ld_fit_method='coupled',
-    max_batch_parameters=25,
-    batch_overlap=2,
-    dlogz=None,
-    maxiter=None,
-    maxcall=None,
-    dynesty_bounding='multi',
-    normalise=True,
-    detrend=True
-    )
+    with suppress_print():
+        firefly(
+        # Firefly Interface
+        exoplanet,
+        archive='eu',
+        curve_sample=1,
+        email=False,
+        to=['transitfit.server@gmail.com'],
+        clean=True,
+        cache=False,
+        auto=True,
+        # TransitFit Variables
+        cutoff=0.25,
+        window=2.5,
+        nlive=1000,
+        fit_ttv=False,
+        detrending_list=[['nth order', 2]],
+        dynesty_sample='rslice',
+        fitting_mode='folded',
+        limb_darkening_model='quadratic',
+        ld_fit_method='coupled',
+        max_batch_parameters=25,
+        batch_overlap=2,
+        dlogz=None,
+        maxiter=None,
+        maxcall=None,
+        dynesty_bounding='multi',
+        normalise=True,
+        detrend=True
+        )
 
 
 main(argv[1:])
