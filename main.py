9bdc93884f2d4bb0f76139034bd828527066d23f
Steven Charles-Mindoza <stevemindoza@gmail.com>
2021-02-04 13:59:42 +0000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,37 @@
+from firefly import firefly
+from sys import argv
+        
+
+def main(exoplanet):
+    firefly(
+    # Firefly Interface
+    exoplanet,
+    archive='eu',
+    curve_sample=1,
+    email=False,
+    to=['transitfit.server@gmail.com'],
+    clean=True,
+    cache=False,
+    auto=True,
+    # TransitFit Variables
+    cutoff=0.25,
+    window=2.5,
+    nlive=1000,
+    fit_ttv=False,
+    detrending_list=[['nth order', 2]],
+    dynesty_sample='rslice',
+    fitting_mode='folded',
+    limb_darkening_model='quadratic',
+    ld_fit_method='coupled',
+    max_batch_parameters=25,
+    batch_overlap=2,
+    dlogz=None,
+    maxiter=None,
+    maxcall=None,
+    dynesty_bounding='multi',
+    normalise=True,
+    detrend=True
+    )
+
+
+main(argv[1:])
