836babef5d9d1a5f472e2dbb9519fa3316fe1264
Steven Charles-Mindoza <stevemindoza@gmail.com>
2021-02-04 14:11:54 +0000

    Update run.py

--- run.py
+++ run.py
@@ -1,26 +1,13 @@
 from firefly import tess
 from multiprocessing import Pool
 from subprocess import run
-import sys
-import os
-
-
-class suppress_print():
-    def __enter__(self):
-        self.original_stdout = sys.stdout
-        sys.stdout = open(os.devnull, 'w')
-
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        sys.stdout.close()
-        sys.stdout = self.original_stdout
-        
 
 def main(exoplanet):
     run(['python', 'main.py', f'{exoplanet}']
     )
 
 # Set how many targets to run in parallel
-processes = 1
+processes = 2
 # Define various lists to pass
 targets, all_targets, ttv_targets = tess(archive='eu', survey='WASP')
 all_targets = [
@@ -32,8 +19,4 @@ all_targets = [
 exoplanet = all_targets
 if __name__ == '__main__':
     with Pool(processes=processes) as pool:
-        if processes==1:
-            pool.map(main, exoplanet)
-        else:
-            with suppress_print():
-                pool.map(main, exoplanet)
+        pool.map(main, exoplanet)
