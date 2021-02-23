a4bae15706169142aff23855cfa848fc9bec53f8
Steven Charles-Mindoza <stevemindoza@gmail.com>
2021-02-23 10:29:23 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from firefly import firefly
 from sys import argv
-import sys, os
+import sys
+import os
+
 
 class suppress_print():
     def __enter__(self):
@@ -10,39 +12,37 @@ class suppress_print():
     def __exit__(self, exc_type, exc_val, exc_tb):
         sys.stdout.close()
         sys.stdout = self.original_stdout
-        
 
 def main(exoplanet):
     with suppress_print():
         firefly(
-            # Firefly Interface
-            exoplanet,
-            archive='eu',
-            curve_sample=1,
-            clean=False,
-            cache=False,
-            auto=True,
-            # MAST Search
-            hlsp=['SPOC'],
-            cadence=[120],
-            # TransitFit Variables
-            cutoff=0.25,
-            window=2.5,
-            nlive=1000,
-            fit_ttv=False,
-            detrending_list=[['nth order', 2]],
-            dynesty_sample='rslice',
-            fitting_mode='folded',
-            limb_darkening_model='quadratic',
-            ld_fit_method='coupled',
-            max_batch_parameters=25,
-            batch_overlap=2,
-            dlogz=None,
-            maxiter=None,
-            maxcall=None,
-            dynesty_bounding='multi',
-            normalise=True,
-            detrend=True
+        # Firefly Interface
+        exoplanet,
+        archive='nasa',
+        curve_sample=1,
+        clean=True,
+        cache=False,
+        auto=True,
+        # TransitFit Variables
+        walks=25,
+        slices=5,
+        cutoff=0.25,
+        window=2,
+        nlive=625,
+        fit_ttv=True,
+        detrending_list=[['nth order', 2]],
+        dynesty_sample='rwalk',
+        fitting_mode='folded',
+        limb_darkening_model='quadratic',
+        ld_fit_method='coupled',
+        max_batch_parameters=25,
+        batch_overlap=2,
+        dlogz=0.01,
+        maxiter=None,
+        maxcall=None,
+        dynesty_bounding='multi',
+        normalise=True,
+        detrend=True
         )
 
 
