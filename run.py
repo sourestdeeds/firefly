0b56d45fcadc3af72d2cc229b3e8e81f21815c4e
Steven Charles-Mindoza <stevemindoza@gmail.com>
2021-03-11 00:06:57 +0000

    Update run.py

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 from firefly import tess
 from multiprocessing import Pool
 from subprocess import run
+import numpy as np
 from pandas import read_csv
 import signal
 
@@ -27,21 +28,24 @@ def main(exoplanet):
         print(f'Finished {exoplanet}')
 
 # Set how many targets to run in parallel
-processes = 1
+processes = 11
 # Define various lists to pass
 # targets, all_targets, ttv_targets = tess(archive='nasa', survey=None)
-all_targets = \
-    read_csv('https://raw.githubusercontent.com/sourestdeeds/' +\
-             'firefly/main/firefly/data/Targets/' +\
-             'nasa_tess_viable.csv?token=ACSJ3DZQMUVPSRCDCVEN4OLAIGBAS') \
-             .sort_values('Epochs', ascending=False)['Exoplanet'].tolist()
+#all_targets = \
+#    read_csv('https://raw.githubusercontent.com/sourestdeeds/' +\
+#             'firefly/main/firefly/data/Targets/' +\
+#             'nasa_tess_viable.csv?token=ACSJ3DZQMUVPSRCDCVEN4OLAIGBAS') \
+#             .sort_values('Epochs', ascending=False)['Exoplanet'].tolist()
 # all_targets = [
 #     'wasp100b', 'wasp126b', 'lhs1815b', 'kepler42c', 'wasp119b',
 #     'wasp18b', 'hip65ab', 'l9859b', 'gj1252b', 'wasp62b',
 #     'toi157b',
 # ]
+spear = read_csv('firefly/data/spear.csv')['pl_name'] .values
+nasa = read_csv('nasa_tess_viable.csv')['Exoplanet'] .values
+diff = np.setdiff1d(nasa, spear)
 # Redefine list to use here
-exoplanet = all_targets[::2]
+exoplanet = diff
 if __name__ == '__main__':
     with Pool(processes=processes) as pool:
         pool.map(main, exoplanet, chunksize=1)
